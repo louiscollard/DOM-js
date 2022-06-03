c34cc42b43fa61b650083c0d87e72a5f597836f8
Louis Collard <louiscollard2@gmail.com>
2022-06-03 12:03:56 +0200

    Finish 04-navigate

---
+++ 04-navigate/script.js
@@ -0,0 +1,23 @@
+/*
+ */
+/******************** 4.1 ********************/
+
+const ol = document.querySelector("ol");
+const olFirstChild = ol.children[0];
+const olLastChild = ol.children[4];
+const olInvertLastAndFirst = ol.insertBefore(olLastChild, olFirstChild);
+
+/******************** 4.2 ********************/
+
+const main = document.querySelector("main");
+const section = document.querySelector("section");
+const thirdSection = section.nextElementSibling;
+let thirdTitle = thirdSection.firstElementChild;
+const secondSection = thirdSection.nextElementSibling;
+let secondTitle = secondSection.firstElementChild.firstElementChild;
+secondSection.appendChild(thirdTitle);
+thirdSection.appendChild(secondTitle);
+
+/******************** 4.3 ********************/
+
+secondSection.removeChild(thirdTitle);
