f6583e1d16f446160145973a79b895b9a9d626dd
Louis Collard <louiscollard2@gmail.com>
2022-06-07 10:17:23 +0200

    Add function square

--- 05-events/script.js
+++ 05-events/script.js
@@ -10,36 +10,23 @@ const ul = document.querySelector("ul");
 const green = document.querySelector(".green");
 const violet = document.querySelector(".violet");
 const orange = document.querySelector(".orange");
-let newLi;
+
+const createSquare = (color) => {
+	const newDiv = document.createElement("div");
+	newDiv.style.display = "inline-block";
+	newDiv.classList.add("displayedsquare", color);
+	displayedSquareWrapper.appendChild(newDiv);
+	let newLi = document.createElement("li");
+	ul.appendChild(newLi);
+	newLi.textContent = `[${getElapsedTime()}] Created a new ${color} square`;
+};
 
 const clickOnSquare = (e) => {
 	console.log(e.target.classList[1]);
 	console.log(getElapsedTime());
-	if (e.target.classList[1] === "green") {
-		const newDiv = document.createElement("div");
-		newDiv.style.display = "inline-block";
-		newDiv.classList.add("displayedsquare", "green");
-		displayedSquareWrapper.appendChild(newDiv);
-		newLi = document.createElement("li");
-		ul.appendChild(newLi);
-		newLi.textContent = `[${getElapsedTime()}] Created a new green square`;
-	} else if (e.target.classList[1] === "violet") {
-		const newDiv = document.createElement("div");
-		newDiv.style.display = "inline-block";
-		newDiv.classList.add("displayedsquare", "violet");
-		displayedSquareWrapper.appendChild(newDiv);
-		newLi = document.createElement("li");
-		ul.appendChild(newLi);
-		newLi.textContent = `[${getElapsedTime()}] Created a new violet square`;
-	} else {
-		const newDiv = document.createElement("div");
-		newDiv.style.display = "inline-block";
-		newDiv.classList.add("displayedsquare", "orange");
-		displayedSquareWrapper.appendChild(newDiv);
-		newLi = document.createElement("li");
-		ul.appendChild(newLi);
-		newLi.textContent = `[${getElapsedTime()}] Created a new orange square`;
-	}
+	if (e.target.classList[1] === "green") createSquare("green");
+	if (e.target.classList[1] === "violet") createSquare("violet");
+	if (e.target.classList[1] === "orange") createSquare("orange");
 };
 
 const actionSquares = document.querySelectorAll(".actionsquare");
@@ -48,14 +35,13 @@ for (let actionSquare of actionSquares) {
 }
 
 body.addEventListener("keydown", (e) => {
-	console.log(e.key);
 	if (e.key === " ") {
 		let r = Math.trunc(Math.random() * 256);
 		let g = Math.trunc(Math.random() * 256);
 		let b = Math.trunc(Math.random() * 256);
 		let bgColor = `rgb( ${r} , ${g}, ${b})`;
 		body.style.background = bgColor;
-		newLi = document.createElement("li");
+		let newLi = document.createElement("li");
 		ul.appendChild(newLi);
 		newLi.textContent = `[${getElapsedTime()}] Created a new background`;
 	} else if (e.key === "l") {
